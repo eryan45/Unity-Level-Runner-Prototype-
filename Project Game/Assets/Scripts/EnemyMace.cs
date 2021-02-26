66d02822fc3c06af307f9eca5f78b8e49968240a
eryan45 <79680892+eryan45@users.noreply.github.com>
2021-02-25 22:14:06 -0600

    Unity Game

---
+++ Project Game/Assets/Scripts/EnemyMace.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class EnemyMace : MonoBehaviour
+{
+
+        [SerializeField] private float amplitude = 2;
+       
+
+    private Vector3 startPos;
+
+    void Start()
+    {
+      
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+    
+        transform.position = startPos + amplitude * new Vector3(0f, Mathf.Sin(Time.time), 0f);
+    }
+}
---
+++ Project Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed, jumpSpeed;
+    [SerializeField] private LayerMask ground;
+    
+    private PlayerActionControls playerActionControls;
+    private Rigidbody2D rb;
+    private Collider2D col;
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    
+    
+    private void Awake() {
+        playerActionControls = new PlayerActionControls();
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+    
+    private void OnEnable() {
+        playerActionControls.Enable();
+    }
+    
+    private void OnDisable() {
+        playerActionControls.Disable();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerActionControls.Land.Jump.performed += _ => Jump();
+    }
+
+
+    private void Jump(){
+        if (IsGrounded()) {
+            rb.AddForce(new Vector2(0, jumpSpeed), ForceMode2D.Impulse);
+            animator.SetTrigger("Jump");
+        }
+    }
+
+    private bool IsGrounded() {
+        Vector2 topLeftPoint = transform.position;
+        topLeftPoint.x -= col.bounds.extents.x;
+        topLeftPoint.y += col.bounds.extents.y;
+        
+        
+        Vector2 bottomRightPoint = transform.position;
+        bottomRightPoint.x += col.bounds.extents.x;
+        bottomRightPoint.y -= col.bounds.extents.y;
+        return Physics2D.OverlapArea(topLeftPoint, bottomRightPoint, ground);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move () {
+         float movementInput = playerActionControls.Land.Move.ReadValue<float>();
+      Vector3 currentPosition = transform.position;
+      currentPosition.x += movementInput * speed * Time.deltaTime;
+      transform.position = currentPosition;
+
+       if (movementInput != 0) animator.SetBool("Run", true);
+       else animator.SetBool("Run", false);
+
+
+        if (movementInput == -1)
+            spriteRenderer.flipX = true;
+        else if (movementInput == 1)
+            spriteRenderer.flipX = false;
+
+     }
+
+        private void OnCollisionEnter2D(Collision2D other) {
+            if (other.gameObject.tag == "Enemy"){
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+
+
+}
